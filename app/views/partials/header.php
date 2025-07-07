dd30a29ea258aaa12b22c2c3856affde77e0e37c
justeozan <juste.ozan@email.com>
2025-07-07 12:20:37 +0200

    create login and register view and userController

---
+++ app/views/partials/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Camagru - Inscription</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+  
+  <!-- Google Font: Pacifico -->
+   <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+
+   <script>
+  tailwind.config = {
+    theme: {
+      extend: {
+        fontFamily: {
+          camagru: ['Pacifico', 'cursive'],
+        }
+      }
+    }
+  }
+</script>
+
+</head>
+<body>
