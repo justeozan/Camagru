c13f69a9b20c45f88477ca6799a654b0134979d6
justeozan <justeoz.an+projects@gmail.com>
2025-07-07 17:32:59 +0200

    feat: implement HomeController and home view; add sidebar for navigation

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        require_once '../app/views/home.php';
+    }
+
+    public function about()
+    {
+        // Code pour la page "À propos"
+    }
+
+}
