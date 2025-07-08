54ec30ac2a53b6cdf6ce17adbd2ae6fb52dfc088
justeozan <juste.ozan@email.com>
2025-07-08 11:47:07 +0200

    feat: implement session management and user verification checks in controllers

--- app/controllers/CreateController.php
+++ app/controllers/CreateController.php
@@ -4,6 +4,7 @@ class CreateController extends Controller
 {
     public function index()
     {
+        $this->requireVerify(); // Require verified user to create content
         require_once '../app/views/create.php';
     }
 
